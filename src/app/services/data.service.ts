1056df680f2b125dd1e91387f370347c51967548
unknown <bionicbiplov45@gmail.com>
2018-11-08 11:20:25 +0700

    change geoserver url

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -145,7 +145,7 @@ export class DataService {
     ]
   };
 
-  private geoserverURL = 'https://203.146.112.253/geoserver/';
+  private geoserverURL = 'https://geoserver.adpc.net/geoserver/';
  // private geoserverURL = 'http://localhost:8080/geoserver/';
 
   getAreaOptions = function () {

1bd68900b6c6e8ae2e384ea627f6494dc1fbd7a4
unknown <bionicbiplov45@gmail.com>
2019-06-21 14:48:41 +0700

    cleanup

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,11 +1,5 @@
+import { formatDate } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-
-export interface Tile {
-  color: string;
-  cols: number;
-  rows: number;
-  text: string;
-}
 
 @Component({
   selector: 'app-about',
@@ -13,14 +7,9 @@ export interface Tile {
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
-  tiles: Tile[] = [
-      {text: 'One', cols: 3, rows: 1, color: 'lightblue'},
-      {text: 'Two', cols: 2, rows: 2, color: 'lightgreen'},
-      {text: 'Three', cols: 1, rows: 2, color: 'lightpink'}
-    ];
-  constructor() { }
 
-  ngOnInit() {
-  }
+  currentYear = formatDate(new Date(), 'yyyy', 'en');
+
+  ngOnInit () { }
 
 }
